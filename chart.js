8211780c6cb4fb82d1dd73e13282570530c1dbf7
DedeSnrwn13 <sunarwandede13@gmail.com>
2019-05-19 16:23:41 +0700

    Rev-8

--- chart.js
+++ chart.js
@@ -5,27 +5,33 @@ var gradientbiru = ctx.createLinearGradient(0, 0, 0, 330);
 var gradientungu = ctx.createLinearGradient(0,0,0,330);
     gradientungu.addColorStop(0, 'rgba(167,167,255,0.7)');   
     gradientungu.addColorStop(1, 'rgba(167,167,255,0)');
+
 var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
-            	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-            	datasets: [{
-                label: 'Total Views',
-                data: [19, 10, 15, 18, 15, 24],//ini datanya,
-                backgroundColor: gradientbiru,//warna bg chart,
-                borderColor: "#a7a7ff", // warna garis,
-                borderWidth: 2 //ketebalan chartnya
-            }]
-            ,{
-            	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-            	datasets: [{
-	                label: 'Product Sold',
-	                data: [15, 8, 17, 18, 15, 10],//ini datanya,
-	                backgroundColor:  gradientungu,//warna bg chart,
-	                borderColor: "#a7a7ff", // warna garis,
-	                borderWidth: 2 //ketebalan chartnya
-            }]
-	}
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+            datasets: [{
+	         	label: 'Total Views',
+	         	data: [15, 10, 15, 18, 15, 24],//ini datanya,
+	         	backgroundColor: gradientbiru,//warna bg chart,
+	         	borderColor: "#a7a7ff", // warna garis,
+	         	borderWidth: 2 //ketebalan chartnya
+        },
+        {
+	        	label: 'Product Sold',
+	         	data: [19, 8, 16, 13, 17, 10],//ini datanya,
+	         	backgroundColor:  gradientungu,//warna bg chart,
+	         	borderColor: "#54d8ff", // warna garis,
+	         	borderWidth: 2 //ketebalan chartnya
+        }
+        ],
+
+	},
+	 options : {
+		   label:{
+		        position: 'bottom'
+		   }
+		}
 
 });
 
